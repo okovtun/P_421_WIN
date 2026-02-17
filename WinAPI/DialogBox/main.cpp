4bd656041355d83fa156f28b69968a249d8d08bb
User <User@DESKTOP-QHG18FL>
2026-02-17 15:26:16 +0300

    Copy in DialogBox DONE

--- WinAPI/DialogBox/main.cpp
+++ WinAPI/DialogBox/main.cpp
@@ -6,9 +6,6 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
-	{
-
-	}
 	return 0;
 }
 
@@ -25,6 +22,19 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;		//Размер буфера 256 Байт
+			CHAR sz_buffer[SIZE] = {};	//Текстовый буфер
+			//Получаем текстовые поля, чтобы к ним можно было обращаться:
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			//Для того чтобы обратиться к какому-либо элементу окна,
+			//ему нужно отправить сообщение:
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);	//считываем текст из поля 'Login'
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);	//загружаем текст из буфера в поле 'Password'
+		}
+		break;
 		case IDOK:
 			MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION);
 			break;
