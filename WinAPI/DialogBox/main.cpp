982a20f7a69ea04a0a115b0da12f1ff80dbee744
User <User@DESKTOP-QHG18FL>
2026-02-19 16:11:51 +0300

    EditFocus DONE

--- WinAPI/DialogBox/main.cpp
+++ WinAPI/DialogBox/main.cpp
@@ -1,6 +1,10 @@
 #include<Windows.h>
 #include"resource.h"
 
+CONST CHAR g_sz_INVITE[] = "¬ведите им€ пользовател€";
+//g_ - Global (глобальна€ переменна€ или константа)
+//sz_ - String Zero (строка, заканчивающа€с€ нулем - NULL Terminated Line)
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -11,17 +15,40 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	//HWND - Handler to Window. 
+	//Ёто указатель, при помощи которого можно обратитьс€ к окну.
+	//(указатель, на который можно отправить сообщение - SendMessage)
+	//Focus - это часть окна, котора€ принимает команды с клавиатуры.
+	//‘окус переключаетс€ клавишей 'Tab' и стрелками.
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
 	}
 	break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_EDIT_LOGIN:
+		{
+			//WPARAM - это DWORD
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (HIWORD(wParam) == EN_SETFOCUS && strcmp(sz_buffer, g_sz_INVITE) == 0)
+				//https://legacy.cplusplus.com/reference/cstring/strcmp/
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
+			if (HIWORD(wParam) == EN_KILLFOCUS && strcmp(sz_buffer, "") == 0)
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
+
+		}
+		break;
 		case IDC_BUTTON_COPY:
 		{
 			CONST INT SIZE = 256;		//–азмер буфера 256 Ѕайт
