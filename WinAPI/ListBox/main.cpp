c7deef940e81c1bdec3311bea52319da0b53b721
User <User@DESKTOP-QHG18FL>
2026-02-26 16:19:54 +0300

    Edit ListBox item DONE

--- WinAPI/ListBox/main.cpp
+++ WinAPI/ListBox/main.cpp
@@ -7,6 +7,7 @@ CONST CHAR* VALUES[] = { "This", "is", "my", "first", "List", "Box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcEdit(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -30,6 +31,10 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_LIST:
+			if (HIWORD(wParam) == LBN_DBLCLK)
+				DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcEdit, 0);
+			break;
 		case IDC_BUTTON_ADD:
 			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcAdd, 0);
 			break;
@@ -78,6 +83,47 @@ BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				HWND hListBox = GetDlgItem(hParent, IDC_LIST);	//Получаем ListBox родителького окна
 				if (SendMessage(hListBox, LB_FINDSTRING, 0, (LPARAM)sz_buffer) == LB_ERR)
 					SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			}
+		}
+		case IDCANCEL:EndDialog(hwnd, 0);
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
+BOOL CALLBACK DlgProcEdit(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	CONST INT SIZE = 256;
+	CHAR sz_buffer[SIZE] = {};
+	HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD);
+	HWND hParent = GetParent(hwnd);
+	HWND hListBox = GetDlgItem(hParent, IDC_LIST);
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)"Изменить выбранный элемент");
+		INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+		SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
+		SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_ADD));
+		SendMessage(hEdit, EM_SETSEL, 0, -1);
+		SendMessage(hEdit, EM_SETSEL, -1, -1);
+	}
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (strlen(sz_buffer) > 0 && SendMessage(hListBox, LB_FINDSTRING,0,(LPARAM)sz_buffer) == LB_ERR)
+			{
+				INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+				SendMessage(hListBox, LB_DELETESTRING, i, 0);
+				SendMessage(hListBox, LB_INSERTSTRING, i, (LPARAM)sz_buffer);
 			}
 		}
 		case IDCANCEL:EndDialog(hwnd, 0);
